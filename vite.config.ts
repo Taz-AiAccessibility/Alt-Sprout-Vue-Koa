3996d6e5c48ec2d6c9971fe916654ec83af15cf9
Ian Buchanan <get.ian.buchanan@gmail.com>
2025-02-07 16:06:43 -0800

    fix(config): add /auth to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,34 @@
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: process.env.VITE_BACKEND_URL || 'http://localhost:3000',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:3000',
+      '/auth': {
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:10000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/api': {
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:10000',
+        changeOrigin: true,
       },
     },
   },
